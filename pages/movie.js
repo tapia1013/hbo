d5f06a8d3e8e677d3bc5b33e51d0d3b85df63f5b
tapia1013 <manbearpig1013@gmail.com>
2021-11-15 20:34:11 -0800

    export static

--- pages/movie.js
+++ pages/movie.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import MainLayout from '../components/Layouts/MainLayout';
 import FeaturedMedia from '../components/UI/FeaturedMedia/FeaturedMedia';
-import ForYouList from '../components/UI/ForYouList/ForYouList';
-import JustAdded from '../components/UI/JustAdded/JustAdded';
 import PosterView from '../components/UI/PosterView/PosterView';
 import CastInfo from '../components/UI/CastInfo/CastInfo';
 
