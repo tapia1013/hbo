89e5ca347c74d232e7c956bc046d47376476a3dd
tapia1013 <manbearpig1013@gmail.com>
2021-11-17 14:44:24 -0800

    optimized poster images with skeleton with smooth lazy loading

--- components/UI/MediaRow/MediaRow.js
+++ components/UI/MediaRow/MediaRow.js
@@ -37,20 +37,24 @@ const MediaRow = (props) => {
   }
 
   // showing the thumbnails
-  const showThumbnails = () => {
+  const showThumbnails = (type) => {
     return loadingData
       ? loopComp((<Skeleton />), 10)
       : movies.map((movie) => {
-        return <Thumbnail movieData={movie} />
+        return <Thumbnail movieData={movie} type={type} />
       })
   }
+
+
+
+
 
   return (
     <div className={`media-row ${props.type}`}>
       <h3 className='media-row__title'>{props.title}</h3>
       <div className='media-row__thumbnails'>
 
-        {showThumbnails()}
+        {showThumbnails(props.type)}
 
         {/* {
           loopComp((<Thumbnail />), 10)
@@ -62,9 +66,24 @@ const MediaRow = (props) => {
 
 
 const Thumbnail = (props) => {
+  const thumbSize = (type) => {
+    if (props.type === 'large-v') {
+      return '400';
+    }
+    if (type === 'small-v') {
+      return '185';
+    }
+    if (type === 'large-h') {
+      return '500';
+    }
+    if (type === 'small-h') {
+      return '342';
+    }
+  }
+
   return (
     <div className="media-row__thumbnail">
-      <img src={`https://image.tmdb.org/t/p/original${props.movieData.poster_path} `} />
+      <img src={`https://image.tmdb.org/t/p/w${thumbSize(props.type)}/${props.movieData.poster_path} `} />
       <div className="media-row__top-layer">
         <i className="fas fa-play" />
       </div>
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,12 @@ import { useEffect } from 'react';
 import { useStateContext } from '../components/HBOProvider';
 import Login from '../components/UI/Login/Login';
 import { useRouter } from 'next/router';
+import LazyLoad from 'react-lazyload';
 import MainLayout from '../components/Layouts/MainLayout';
 import FeaturedMedia from '../components/UI/FeaturedMedia/FeaturedMedia';
-// import ForYouList from '../components/UI/ForYouList/ForYouList';
-// import JustAdded from '../components/UI/JustAdded/JustAdded';
-// import PosterView from '../components/UI/PosterView/PosterView';
 import MediaRow from '../components/UI/MediaRow/MediaRow';
 import AuthCheck from '../components/AuthCheck';
+import Placeholders from '../components/UI/Placeholders/Placeholders';
 
 
 
@@ -28,36 +27,48 @@ export default function Home() {
   return AuthCheck(
     <MainLayout>
       <FeaturedMedia />
-      <MediaRow
-        title="Movies"
-        type="large-v"
-        endpoint="discover/movie?sort_by=popularity.desc&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Series"
-        type="small-h"
-        endpoint="discover/tv?primary_release_year=2021"
-      />
-      <MediaRow
-        title="Action"
-        type="small-v"
-        endpoint="discover/movie?with_genres=28&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Horror"
-        type="small-v"
-        endpoint="discover/movie?with_genres=27&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Animation"
-        type="large-h"
-        endpoint="discover/movie?with_genres=16&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Sci-Fi"
-        type="small-v"
-        endpoint="discover/movie?with_genres=878&primary_release_year=2021"
-      />
+      <LazyLoad offset={-400} placeholder={<Placeholders title="Movies" type="large-v" />}>
+        <MediaRow
+          title="Movies"
+          type="large-v"
+          endpoint="discover/movie?sort_by=popularity.desc&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad offset={-400} placeholder={<Placeholders title="Series" type="small-h" />}>
+        <MediaRow
+          title="Series"
+          type="small-h"
+          endpoint="discover/tv?primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad offset={-400} placeholder={<Placeholders title="Action" type="small-v" />}>
+        <MediaRow
+          title="Action"
+          type="small-v"
+          endpoint="discover/movie?with_genres=28&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad offset={-400} placeholder={<Placeholders title="Horror" type="small-v" />}>
+        <MediaRow
+          title="Horror"
+          type="small-v"
+          endpoint="discover/movie?with_genres=27&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad offset={-400} placeholder={<Placeholders title="Animation" type="large-h" />}>
+        <MediaRow
+          title="Animation"
+          type="large-h"
+          endpoint="discover/movie?with_genres=16&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad offset={-400} placeholder={<Placeholders title="Sci-Fi" type="small-v" />}>
+        <MediaRow
+          title="Sci-Fi"
+          type="small-v"
+          endpoint="discover/movie?with_genres=878&primary_release_year=2021"
+        />
+      </LazyLoad>
     </MainLayout>
   );
 }
